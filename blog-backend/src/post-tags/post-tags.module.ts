1aba6dcb9938ab16dd76177055159c4c6cffc7e4
vuongtrungit9x <vuongtrungit9x@gmail.com>
2024-05-20 14:20:34 +0700

    refactor: clean unsued code

--- blog-backend/src/post-tags/post-tags.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostTagsController } from './post-tags.controller';
-
-@Module({
-  controllers: [PostTagsController],
-})
-export class PostTagsModule {}
